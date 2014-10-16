d014798efe7e21480605faf335ea58b0e2281421
snowfalcon.z <snowfalcon.z@gmail.com>
2014-10-16 10:45:23 +0000

    initial commit

---
+++ ntu-fyp2014-physics-engine/src/sg/edu/ntu/aalhossary/fyp2014/physics_engine/BoundingBox.java
@@ -0,0 +1,7 @@
+package sg.edu.ntu.aalhossary.fyp2014.physics_engine;
+public class BoundingBox extends BoundingPrimitive {
+
+	public boolean overlap(BoundingPrimitive aBs) {
+		return false;
+	}
+}
---
+++ ntu-fyp2014-physics-engine/src/sg/edu/ntu/aalhossary/fyp2014/physics_engine/BoundingPrimitive.java
@@ -0,0 +1,5 @@
+package sg.edu.ntu.aalhossary.fyp2014.physics_engine;
+public abstract class BoundingPrimitive {
+
+	public abstract boolean overlap(BoundingPrimitive aBs);
+}
